9aa75ff3bfdc64a115f743913d197233bd45b7c9
Alex <alexbachurin91@gmail.com>
2021-09-28 23:42:30 +0300

    display placeholder image if object from data doesnt have one

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 const SingleMovie = ({ Poster, Title, Year, imdbID }) => {
+    //check if Poster value = 'N/A' we place placeholder in src
+    let imgSrc = ''
+    if (Poster === 'N/A') {
+        imgSrc = 'https://res.cloudinary.com/dljezd6qv/image/upload/v1632861499/No-Image-Placeholder.svg.png'
+    } else {
+        imgSrc = Poster;
+    }
     return (
         <article className="movie">
-            <img className="movie-img" src={Poster} alt={Title} />
+            <img className="movie-img" src={imgSrc} alt={Title} />
             <div className="movie-info">
                 <h4 className="movie-title">{Title}</h4>
                 <p className="movie-year">{Year}</p>

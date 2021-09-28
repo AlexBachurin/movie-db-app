200a866a33eb2eb3db4d3adbd6569d305bc24171
Alex <alexbachurin91@gmail.com>
2021-09-28 15:33:45 +0300

    html/css setup

---
+++ src/components/Movies.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Movies = () => {
+    return (
+        <section className="movies">
+            <article className="movie">
+                <img className="movie-img" src="https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="batman" />
+                <div className="movie-info">
+                    <h4 className="movie-title">Title</h4>
+                    <p className="movie-year">year</p>
+                </div>
+            </article>
+            <article className="movie">
+                <img className="movie-img" src="https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="batman" />
+                <div className="movie-info">
+                    <h4 className="movie-title">Title</h4>
+                    <p className="movie-year">year</p>
+                </div>
+            </article>
+            <article className="movie">
+                <img className="movie-img" src="https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg" alt="batman" />
+                <div className="movie-info">
+                    <h4 className="movie-title">Title</h4>
+                    <p className="movie-year">year</p>
+                </div>
+            </article>
+        </section>
+    )
+}
+
+export default Movies
---
+++ src/components/SearchForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchForm = () => {
+    return (
+        <form className="search-form">
+            <h2>Search movies</h2>
+            <input className="search-input" type="text" />
+        </form>
+    )
+}
+
+export default SearchForm

74d9b6ead61f8b1899e2b7433e65ba471e773160
Alex <alexbachurin91@gmail.com>
2021-09-28 23:52:50 +0300

    loading display as image

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
     return (
         <div className="loading">
-            Loading...
+            <img src="https://res.cloudinary.com/dljezd6qv/image/upload/v1632862166/Loading_dark-bg.svg" alt="loading" />
         </div>
     )
 }

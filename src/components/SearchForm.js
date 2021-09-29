157ec3fb9d488ea91dd22ac91fff1132e571dd1d
Alex <alexbachurin91@gmail.com>
2021-09-29 19:43:05 +0300

    prevent page refresh on search enter click

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,7 @@ import { useGlobalContext } from '../context'
 const SearchForm = () => {
     const { term, handleSearch } = useGlobalContext();
     return (
-        <form className="search-form">
+        <form onSubmit={(e) => e.preventDefault()} className="search-form">
             <h2>Search movies</h2>
             <input onChange={handleSearch} value={term} className="search-input" type="text" />
         </form>
